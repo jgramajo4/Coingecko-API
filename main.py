4f918f092ad920a504fc0820514d4e65d4049606
Juan Gramajo Perez <jgramajo4@protonmail.com>
2021-05-27 23:57:44 +0000

    learned how to properly send one parameter to an end point. Still working on the formatting for multiple parameters.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import json
 import requests
 
-response = requests.get('api.coingecko.com/api/v3/coins/list')
-print(response)
+parameters = {
+  "ids": 'bitcoin',
+  "vs_currencies": 'usd',
+  "include_24hr_change": 'true'
+}
+
+response = requests.get("https://api.coingecko.com/api/v3/simple/price", params=parameters)
+print(response.json())
